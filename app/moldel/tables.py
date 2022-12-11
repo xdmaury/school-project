fbad781af18c638fcd7c7dc2f194f4e7fc804e75
xdmaury <maury1418@hotmail.com>
2022-12-10 23:07:16 -0300

    creating student class and course

---
+++ app/moldel/tables.py
@@ -0,0 +1,25 @@
+from app import db
+
+
+class Student(db.Model):
+
+    __tablename__ = "student"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String)
+    course = db.Column(db.String)
+
+    def __int__(self, name, email, course):
+        self.name = name
+        self.email = email
+        self.course = course
+
+
+class Course(db.Model):
+
+    __tablename__ = "course"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    
